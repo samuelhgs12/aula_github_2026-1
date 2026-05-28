aa458a6c93c6801f8acd17f398802f618a872b9d
lucasvsts <lucasvianasts@gmail.com>
2026-05-27 22:20:08 -0300

    close issue #2

--- Menu.java
+++ Menu.java
@@ -1,5 +1,6 @@
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -38,6 +39,10 @@ public class Menu {
 			if (op>=i){
 				System.out.println("Opcao errada!");
 				op=0;
+			}
+			else if (options.get(op - 1).equals("Conta")) {
+				Menu contaMenu = new Menu("Conta", Arrays.asList("Abrir Conta", "Encerrar Conta", "Consultar Conta"));
+				op = contaMenu.getSelection();
 			}
 
 		}
