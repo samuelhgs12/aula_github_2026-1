59fc0858c3122142921bc26ca6a65a2acbd3f2dc
kayke <kaykecristofersc@gmail.com>
2026-05-27 22:07:48 -0300

    feat: adição do cadastro de cliente

--- Main.java
+++ Main.java
@@ -1,11 +1,35 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		Menu mainMenu =  new Menu("Menu Principal", Arrays.asList("Conta", "Cliente", "Operacoes"));
-		System.out.println(mainMenu.getSelection() + "foi selecionada");
+		int opcao = mainMenu.getSelection();
+		System.out.println(opcao + " foi selecionada");
+		
+		if (opcao == 2) {
+			cadastrarCliente();
+		}
+		
 		System.out.println("Fim");
+	}
+	
+	public static void cadastrarCliente() {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("\n=== CADASTRO DE CLIENTE ===\n");
+		
+		System.out.print("Informe o nome do cliente: ");
+		String nome = scanner.nextLine();
+		
+		System.out.print("Informe o CPF do cliente: ");
+		String cpf = scanner.nextLine();
+		
+		System.out.println("\n--- Cliente cadastrado com sucesso ---");
+		System.out.println("Nome: " + nome);
+		System.out.println("CPF: " + cpf);
+		System.out.println();
 	}
 
 }
